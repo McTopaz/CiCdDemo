6573109ac0b435d7d46040e10c535baace2661f6
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-11-25 20:39:39 +0100

    Working class library.

---
+++ Library/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Library
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b) { return a + b; }
+        public int Subtraction(int a, int b) {return a - b; }
+        public int Multiplication(int a, in int b) {return a * b;}
+        public int Division(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException($"Cannot devide. Denominator is zero.");
+
+            return a / b;
+        }
+    }
+}
