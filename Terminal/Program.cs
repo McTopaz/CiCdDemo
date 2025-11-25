793f8964e6b994523e484d61957c12019291d645
McTopaz <stefan.k.j.eriksson@gmail.com>
2025-11-25 20:44:35 +0100

    Working commit.

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Library;
+
+var calculator = new Calculator();
+
+var a = calculator.Addition(1, 2);
+var b = calculator.Subtraction(3, 1);
+var c = calculator.Multiplication(2, 3);
+var d = calculator.Division(10, 2);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
